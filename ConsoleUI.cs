a37d15a8c5d744bd7ce401ad26e6bb7db807bb06
Marjaana Heikkinen <piritamarjaana@gmail.com>
2019-10-23 15:27:20 +0300

    console ui created

---
+++ ConsoleUI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RataDigiTraffic.Model;
+using RataDigiTraffic;
+
+namespace Trains
+{
+    class ConsoleUI
+    {
+        
+
+        public static void UserInput()
+        {
+            SearchLogic.PopulateStationDictionary();
+            Console.WriteLine("Welcome to TrainSearch!");
+            Console.WriteLine("");
+
+            Console.Write("from:");
+           
+            string from = Console.ReadLine().ToUpper().Trim();
+
+            Console.Write("to:");
+
+            string to = Console.ReadLine().ToUpper().Trim();
+
+
+
+
+
+
+            try
+            {
+                var fromStation = SearchLogic.ConvertUserInputStringToStation(from);
+                var toStation = SearchLogic.ConvertUserInputStringToStation(to);
+                SearchLogic.SearchBetweenStations(fromStation, toStation);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message); 
+            }
+           
+                
+            
+            
+
+            
+
+
+
+
+           
+            
+
+            if (SearchLogic.stationDictionary.ContainsKey(to))
+
+            {
+                Console.WriteLine(SearchLogic.stationDictionary[to]);
+            }
+
+          
+            Console.WriteLine("");
+
+            //SearchLogic.SearchBetweenStations(from, to);
+            SearchLogic.GetTrainRoute();
+
+
+            //Console.Write("Search train:");
+            //string trainSearch = Console.ReadLine();
+            //Console.Write("Search station:");
+            //string stationSearch = Console.ReadLine();
+
+
+
+        }
+
+
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -10,19 +10,31 @@ namespace Trains
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Haetaan asemia...");
-            SearchLogic.PopulateStationDictionary();
-            Console.WriteLine(SearchLogic.ConvertUserInputStationToShortCode("eno"));
+            ConsoleUI ui = new ConsoleUI();
+
+            ConsoleUI.UserInput();
 
 
-            var from = new Station();
-            from.stationShortCode = "HKI";
-            from.stationName = "Helsinki";
-            var to = new Station();
-            to.stationShortCode = "PSL";
-            to.stationName = "Pasila";
-            SearchLogic.SearchBetweenStations(from, to);
-            SearchLogic.GetTrainRoute();
+
+
+
+
+
+            //Console.WriteLine("Haetaan asemia...");
+            //SearchLogic.PopulateStationDictionary();
+            //Console.WriteLine(SearchLogic.ConvertUserInputStationToShortCode("eno"));
+
+
+            //var from = new Station();
+            //from.stationShortCode = "HKI";
+            //from.stationName = "Helsinki";
+
+            //var to = new Station();
+            //to.stationShortCode = "PSL";
+            //to.stationName = "Pasila";
+
+            //SearchLogic.SearchBetweenStations(from, to);
+            //SearchLogic.GetTrainRoute();
         }
     }
 }
